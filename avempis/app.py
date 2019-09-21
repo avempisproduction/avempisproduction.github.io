c86a9cd55a93cdcbf7382bfa84ce1cd9db6df6de
azzamsa <azzam@azzamsa.com>
2019-09-21 21:50:28 +0700

    Add freezer

--- avempis/app.py
+++ avempis/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+freezer = Freezer(app)
 
 
 @app.route("/")
---
+++ freeze.py
@@ -0,0 +1,4 @@
+from avempis import app
+
+if __name__ == '__main__':
+    app.freezer.freeze()
